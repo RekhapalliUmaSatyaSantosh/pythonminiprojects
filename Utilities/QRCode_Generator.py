597748a8e5ea69bd8ce860dc532e067782f56666
Satya Santosh <166365853+RekhapalliUmaSatyaSantosh@users.noreply.github.com>
2026-07-17 19:05:44 +0530

    Update QRCode_Generator.py

--- Utilities/QRCode_Generator.py
+++ Utilities/QRCode_Generator.py
@@ -1,8 +1,12 @@
 import qrcode
+
 data=input('Enter the text or url=').strip()
 filename=input('Enter the filename=').strip()
+
 qr=qrcode.QRCode(box_size=10,border=4)
 qr.add_data(data)
+
 img=qr.make_image(fill_color='black', back_color='white')
 img.save(filename)
+
 print(f'QR code saved to={filename}')
