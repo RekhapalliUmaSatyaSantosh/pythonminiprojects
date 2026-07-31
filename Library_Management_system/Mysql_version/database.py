768b56b01619d73174e490d5a02eb7d04115e603
Satya Santosh <166365853+RekhapalliUmaSatyaSantosh@users.noreply.github.com>
2026-07-31 09:48:57 +0530

    Hide database password in database.py
    Updated database connection password to hide sensitive information.

--- Library_Management_system/Mysql_version/database.py
+++ Library_Management_system/Mysql_version/database.py
@@ -5,7 +5,7 @@ class Database:
         self.conn = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='Santosh@1503',
+            password='*********',
             database='library'
         )
         self.cursor=self.conn.cursor()
